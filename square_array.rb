d1bdf291dad58c2664c9435b84ade86b2e3262ca
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-11 22:23:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,16 +4,3 @@ def square_array(array)
  return random_array
 end
  
-  #square_array(numbers)
-  #numbers.each do
-   # |number| number ** 2
-  #end
-#end
-
-#def square_array(numbers)
-  #new_numbers = []
-  #numbers.each do |num|
-    #new_numbers << (num ** 2)
-  #end
-  #return new_numbers
-#end
